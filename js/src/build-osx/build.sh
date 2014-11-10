bf8f970dabc135037cc0762dc8e335c3ac4abf9e
PeyTy <alegorium@gmail.com>
2014-11-10 17:10:08 +0400

    build script clang-edition
    - no JIT

--- js/src/build-osx/build.sh
+++ js/src/build-osx/build.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
+echo ""
+echo "Building build-osx-x86:"
+
+# Go into current location:
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CD=$DIR
+cd $CD
+
 cpus=$(sysctl hw.ncpu | awk '{print $2}')
 
 # configure
-../configure --disable-tests --disable-shared-js \
+CC=clang CXX="clang++ -m32" CXXFLAGS="-m32" ../configure --disable-tests --disable-shared-js \
             --enable-strip --enable-strip-install \
-            --disable-root-analysis --disable-exact-rooting --enable-gcincremental --enable-optimize=-O3 \
-            --enable-llvm-hacks \
+            --disable-root-analysis --disable-exact-rooting --enable-gcincremental --enable-optimize=-O1 \
+            --enable-llvm-hacks --disable-methodjit --disable-ion --disable-yarr-jit \
             --disable-debug \
             --without-intl-api \
             --disable-threadsafe
@@ -19,3 +27,7 @@ xcrun strip -S libjs_static.a
 # info
 xcrun lipo -info libjs_static.a
 
+#
+# done
+#
+echo "*** DONE ***"
